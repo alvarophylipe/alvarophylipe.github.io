7a4e61f0a4c8cbb1e123029ea583f28ffaece6ec
alvarophylipe <aphylipe44@gmail.com>
2022-08-07 19:22:44 -0300

    Resolution Desktop Update

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,28 @@
 function not_added_item(){
-    alert('Not Added Item!')
+    alert('Not Added Item!');
 }
+
+function size_of_screen () {
+    var screenWidth = window.innerWidth;
+    var screenHeight = screen.height;
+    const resolution = document.querySelector('#css');
+    document.querySelector('.screen-size').innerHTML = screenWidth + 'x' + screenHeight + resolution.href;
+}
+
+function autoResize () {
+    screenWidth = screen.width;
+    windowWidth = window.innerWidth;
+    const resolution = document.querySelector('#css');
+    if (windowWidth <= 1366 || screenWidth <= 1366) {
+        resolution.href = 'assets/css/desktopHD.css';
+    }
+    else if (windowWidth <= 1920 || screenWidth <= 1920) {
+        resolution.href = 'assets/css/desktopFULLHD.css';
+    }
+};
+
+window.addEventListener('resize', function(){;
+    autoResize();
+});
+
+
